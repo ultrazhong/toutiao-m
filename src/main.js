f6ec6825c5e8f4ac1c8d7bd6cc84221d850a3240
ultrazhong <zs.dawn@outlook.com>
2021-01-09 17:08:32 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//加载全局样式
+import './styles/index.less'
+
+import Vue from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
+//创建 vue 根实例
 new Vue({
   router,
   store,
